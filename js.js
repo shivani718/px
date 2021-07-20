1a8d0ac056f00a20bf65f85676e8cb16c507030f
shivani718 <59058859+shivani718@users.noreply.github.com>
2021-07-20 15:16:49 +0530

    Update js.js

--- js.js
+++ js.js
@@ -8,11 +8,49 @@
 
 function login()
     {
-        var userName=document.getElementById("username");
-        var password=document.getElementById("password");
-        if(userName.value=="admin" &&password.value =="admin")
+        var userName=document.getElementById("username").value;
+        var password=document.getElementById("password").value;
+      const myaccount = {
+        a1: "PX_Company-1",
+        a2: "PX_Company-2",
+        a3: "PX_Company-3",
+        a4: "PX_Company-4"
+
+    }
+        if(userName=="shivani.reddy00@gmail.com"||userName=="shivanicheemala99@gmail.com" &&password=="admin")
         {
-            location.href ="home.html";
+          b = username.substr(8, 6);
+            var id = b;
+        if(userName=="shivani.reddy00@gmail.com"){
+          
+         //passing user and account objects:
+aptrinsic("identify",
+  {
+  //User Fields
+    "id": id // Required for logged in app users
+    "email": userName
+  },
+  {
+  //Account Fields
+    "id": myaccount.a4, //Required
+    "name": myaccount.a4,
+ });
+        }else if((userName=="shivanicheemala99@gmail.com"){
+          
+         //passing user and account objects:
+aptrinsic("identify",
+  {
+  //User Fields
+    "id": id // Required for logged in app users
+    "email": userName
+  },
+  {
+  //Account Fields
+    "id": myaccount.a3, //Required
+    "name": myaccount.a3,
+ });
+        }
+         location.href ="home.html";
         }
         else
         {
