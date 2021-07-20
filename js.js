8bbc843e91cd564f42f390095d8c71e3ceb01c94
shivani718 <59058859+shivani718@users.noreply.github.com>
2021-07-20 14:52:50 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,10 @@
 
-  <script type="text/javascript">
-  (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
+
+  function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-EMCLD0J9SEPQ-2");
-</script>
+  }(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-EMCLD0J9SEPQ-2");
 
 function login()
     {
