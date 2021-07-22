e585b23a07aa175fd4430cee630d4f9c38d2b970
shivani718 <59058859+shivani718@users.noreply.github.com>
2021-07-22 22:30:43 +0530

    Update js.js

--- js.js
+++ js.js
@@ -100,3 +100,81 @@ function deleteAllCookies() {
     window.aptrinsic('reset');
     counter = 0;
 }
+
+var Productname = "T.V";
+var Productbrand = "Panasonic";
+var Productprice = 90000;
+var transactionStatus = "Success";
+
+
+let purchaseinitiated = new CustomEvent('purchaseinitiated', {
+    detail: {
+        itemName: this.Productname,
+        itemBrand: this.Productbrand
+    }
+})
+let transactionsuccess = new CustomEvent('transactionsuccess', {
+    detail: {
+        Amountdeducted: this.Productprice,
+        Paymentstatus: this.transactionStatus
+    }
+})
+document.addEventListener('DOMContentLoaded', function () {
+    let m = document.getElementById('paymentbtn');
+    addButton(m);
+    m.addEventListener('click', function (ev) {
+
+        addPaymentStatus(m);
+    });
+
+});
+
+function addButton(parent) {
+    let b = document.createElement('button');
+    b.setAttribute("id", "Buynow");
+    b.setAttribute("class", "btn btn-primary");
+    b.textContent = "Make Payment";
+    parent.appendChild(b);
+    return b;
+}
+
+function addPaymentStatus(parent) {
+    let p = document.createElement('p');
+    p.textContent = "Your Transaction is being Proccessed.....";
+    p.setAttribute("id", "tStatus");
+    parent.appendChild(p);
+    p.addEventListener('purchaseinitiated', purchasedone);
+    p.dispatchEvent(purchaseinitiated);
+    setTimeout(printreciept, 2000);
+
+
+}
+function purchasedone(ev) {
+
+    console.log(ev.type, ev.detail);
+    // Write your PX code here to track the custom events 
+    aptrinsic('track', 'purchaseinitiated', {
+        itemName: ev.detail.itemName,
+        itemBrand: ev.detail.itemBrand
+    });
+}
+
+function printreciept() {
+    var p1 = document.getElementById("tStatus");
+    document.addEventListener('transactionsuccess', transactiondone);
+    document.dispatchEvent(transactionsuccess);
+    p1.textContent = "Transaction Success";
+
+}
+
+
+function transactiondone(ev1) {
+
+    console.log(ev1.type, ev1.detail);
+    // Write your PX code here to track the custom events
+    aptrinsic('track', 'transactionsuccess', {
+        Amountdeducted: ev1.detail.Amountdeducted,
+        Paymentstatus: ev1.detail.Paymentstatus
+    });
+}
+
