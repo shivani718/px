e31827aa6c4227ab5d3580b2f8139f0e20547fcd
shivani718 <59058859+shivani718@users.noreply.github.com>
2021-12-29 19:09:20 +0530

    Update js.js

--- js.js
+++ js.js
@@ -11,7 +11,6 @@
     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
   })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-9H6YVGUF2KZV-2");
-</script>
 <!-- Gainsight PX Tag-->
 
 //segment
