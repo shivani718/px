925c526b349a426f6beaed80e4e9664f0ad0bb80
shivani718 <59058859+shivani718@users.noreply.github.com>
2022-03-03 18:13:02 +0530

    Update js.js

--- js.js
+++ js.js
@@ -3,7 +3,6 @@
 // <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/enc-base64.min.js"/>
 // <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/hmac-sha256.min.js"></script>
 
-const lib =  require('./https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/hmac-sha256.min.js');
 
 (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
