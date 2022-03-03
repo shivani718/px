e2d7ce2dc1845703727caefe6a5ce937ad65afb5
shivani718 <59058859+shivani718@users.noreply.github.com>
2022-03-03 17:16:30 +0530

    Update js.js

--- js.js
+++ js.js
@@ -6,9 +6,9 @@
 //     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
 //   })(window,document,"https://web-sdk-eu.aptrinsic.com/api/aptrinsic.js","AP-A0TCSVARXRXO-2");
 // <!-- Gainsight PX Tag-->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.min.js","https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/enc-base64.min.js","https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/hmac-sha256.min.js"></script>
 // <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/enc-base64.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/hmac-sha256.min.js"></script>
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/hmac-sha256.min.js"></script>
 
 (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
