d23df35e7827a5cd858ecb8e371ed2cad5b76e00
shivani718 <59058859+shivani718@users.noreply.github.com>
2022-03-03 17:19:10 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,11 +1,4 @@
-//S_EU tag key
-// <!-- Gainsight PX Tag-->
-//   (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
-//       (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
-//     var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
-//     var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-//   })(window,document,"https://web-sdk-eu.aptrinsic.com/api/aptrinsic.js","AP-A0TCSVARXRXO-2");
-// <!-- Gainsight PX Tag-->
+
 <div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/enc-base64.min.js"></script>
