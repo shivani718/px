b308f9ff42ffe63d58e48d6644b053ea11839ff0
shivani718 <59058859+shivani718@users.noreply.github.com>
2022-03-08 23:21:47 +0530

    Update js.js

--- js.js
+++ js.js
@@ -113,192 +113,192 @@ var hashUserId = String(CryptoJS.HmacSHA256(idforhash, "oceNJBXdda80ZBLaggCrza0a
 //   iframe tag configure---end
 
 
-function validate(username)
-{
-var username=document.getElementById("username").value;
-var password=document.getElementById("password").value;
-    const myaccount = {
-        a1: "PX_Company-1",
-        a2: "PX_Company-2",
-        a3: "PX_Company-3",
-        a4: "PX_Company-4"
+// function validate(username)
+// {
+// var username=document.getElementById("username").value;
+// var password=document.getElementById("password").value;
+//     const myaccount = {
+//         a1: "PX_Company-1",
+//         a2: "PX_Company-2",
+//         a3: "PX_Company-3",
+//         a4: "PX_Company-4"
 
-    }
+//     }
 
-    if(username!=""){
-        if((username=="vinesh.gandhe@gmail.com" || username=="gvk7663@gmail.com" || username=="vgande@gainsight.com"||username=="demouser7663@gmail.com" || username=="scheemala@gainsight.com" || username=="shivanicheemala99@gmail.com")&& password=="user" )
-        {
-            alert("valid user");
-            b = username.substr(6, 5);
-            var id = b;
-//               var attrib_value= document.getElementById("myCheck").checked;
-            if (username=="vinesh.gandhe@gmail.com") {
-                    aptrinsic('identify', {
-                            //User Fields
-                            "id": id, // Required for logged in app users
-                            "email": username,
-                            "gender": "male",
-//                             "useractive" :attrib_value,
-//                             "checkattrib":attrib_value,
-                            "customtest":"{working:yes,default:no,license:free}",
+//     if(username!=""){
+//         if((username=="vinesh.gandhe@gmail.com" || username=="gvk7663@gmail.com" || username=="vgande@gainsight.com"||username=="demouser7663@gmail.com" || username=="scheemala@gainsight.com" || username=="shivanicheemala99@gmail.com")&& password=="user" )
+//         {
+//             alert("valid user");
+//             b = username.substr(6, 5);
+//             var id = b;
+// //               var attrib_value= document.getElementById("myCheck").checked;
+//             if (username=="vinesh.gandhe@gmail.com") {
+//                     aptrinsic('identify', {
+//                             //User Fields
+//                             "id": id, // Required for logged in app users
+//                             "email": username,
+//                             "gender": "male",
+// //                             "useractive" :attrib_value,
+// //                             "checkattrib":attrib_value,
+//                             "customtest":"{working:yes,default:no,license:free}",
                             
 
-                            // "userHash": hash.toString()// optional transient for HMAC identification
-                        },
+//                             // "userHash": hash.toString()// optional transient for HMAC identification
+//                         },
 
-                        {
-                            //Account Fields
-                            "id": myaccount.a4, //Required
-                            "name": myaccount.a4,
-                            // flat custom attributes
-                        },
+//                         {
+//                             //Account Fields
+//                             "id": myaccount.a4, //Required
+//                             "name": myaccount.a4,
+//                             // flat custom attributes
+//                         },
                         
-                        );
-                } else if (username=="gvk7663@gmail.com") {
-                    aptrinsic('identify', {
-                            //User Fields
-                            "id": id, // Required for logged in app users
-                            "email": username
+//                         );
+//                 } else if (username=="gvk7663@gmail.com") {
+//                     aptrinsic('identify', {
+//                             //User Fields
+//                             "id": id, // Required for logged in app users
+//                             "email": username
 
-                            // "userHash": hash.toString()// optional transient for HMAC identification
-                        },
+//                             // "userHash": hash.toString()// optional transient for HMAC identification
+//                         },
 
-                        {
-                            //Account Fields
-                            "id": myaccount.a3, //Required
-                            "name": myaccount.a3,
-                            // flat custom attributes
-                        });
-                } else if (username=="vgande@gainsight.com") {
-                    aptrinsic('identify', {
-                            //User Fields
-                            "id": id, // Required for logged in app users
-                            "email": username
+//                         {
+//                             //Account Fields
+//                             "id": myaccount.a3, //Required
+//                             "name": myaccount.a3,
+//                             // flat custom attributes
+//                         });
+//                 } else if (username=="vgande@gainsight.com") {
+//                     aptrinsic('identify', {
+//                             //User Fields
+//                             "id": id, // Required for logged in app users
+//                             "email": username
 
-                            // "userHash": hash.toString()// optional transient for HMAC identification
-                        },
+//                             // "userHash": hash.toString()// optional transient for HMAC identification
+//                         },
 
-                        {
-                            //Account Fields
-                            "id": myaccount.a2, //Required
-                            "name": myaccount.a2,
-                            // flat custom attributes
-                        });
-                } else {
-                    aptrinsic('identify', {
-                            //User Fields
-                            "id": id, // Required for logged in app users
-                            "email": username
+//                         {
+//                             //Account Fields
+//                             "id": myaccount.a2, //Required
+//                             "name": myaccount.a2,
+//                             // flat custom attributes
+//                         });
+//                 } else {
+//                     aptrinsic('identify', {
+//                             //User Fields
+//                             "id": id, // Required for logged in app users
+//                             "email": username
 
-                            // "userHash": hash.toString()// optional transient for HMAC identification
-                        },
+//                             // "userHash": hash.toString()// optional transient for HMAC identification
+//                         },
 
-                        {
-                            //Account Fields
-                            "id": myaccount.a1, //Required
-                            "name": myaccount.a1,
-                            // flat custom attributes
-                        });
-                }
+//                         {
+//                             //Account Fields
+//                             "id": myaccount.a1, //Required
+//                             "name": myaccount.a1,
+//                             // flat custom attributes
+//                         });
+//                 }
 
-            alert("Logged in user id :" + b);
-            //window.open("/HTML/home.html");
-            window.location = "https://shivani718.github.io/px/home.html";
+//             alert("Logged in user id :" + b);
+//             //window.open("/HTML/home.html");
+//             window.location = "https://shivani718.github.io/px/home.html";
       
-            return false;
+//             return false;
 
-        }
-        else
-        {
-            alert("login failed");
-        }
-    }
-        else{
-            alert("enter 'demouser7663@gmail.com'");
-        }
+//         }
+//         else
+//         {
+//             alert("login failed");
+//         }
+//     }
+//         else{
+//             alert("enter 'demouser7663@gmail.com'");
+//         }
 
 
-}
+// }
 //to clear the cookies after logout
 function deleteAllCookies() {
     window.aptrinsic('reset');
     counter = 0;
 }
 
-var Productname = "T.V";
-var Productbrand = "Panasonic";
-var Productprice = 90000;
-var transactionStatus = "Success";
+// var Productname = "T.V";
+// var Productbrand = "Panasonic";
+// var Productprice = 90000;
+// var transactionStatus = "Success";
 
 
-let purchaseinitiated = new CustomEvent('purchaseinitiated', {
-    detail: {
-        itemName: this.Productname,
-        itemBrand: this.Productbrand
-    }
-})
-let transactionsuccess = new CustomEvent('transactionsuccess', {
-    detail: {
-        Amountdeducted: this.Productprice,
-        Paymentstatus: this.transactionStatus
-    }
-})
-document.addEventListener('DOMContentLoaded', function () {
-    let m = document.getElementById('paymentbtn');
-    addButton(m);
-    m.addEventListener('click', function (ev) {
+// let purchaseinitiated = new CustomEvent('purchaseinitiated', {
+//     detail: {
+//         itemName: this.Productname,
+//         itemBrand: this.Productbrand
+//     }
+// })
+// let transactionsuccess = new CustomEvent('transactionsuccess', {
+//     detail: {
+//         Amountdeducted: this.Productprice,
+//         Paymentstatus: this.transactionStatus
+//     }
+// })
+// document.addEventListener('DOMContentLoaded', function () {
+//     let m = document.getElementById('paymentbtn');
+//     addButton(m);
+//     m.addEventListener('click', function (ev) {
 
-        addPaymentStatus(m);
-    });
+//         addPaymentStatus(m);
+//     });
 
-});
+// });
 
-function addButton(parent) {
-    let b = document.createElement('button');
-    b.setAttribute("id", "Buynow");
-    b.setAttribute("class", "btn btn-primary");
-    b.textContent = "Make Payment";
-    parent.appendChild(b);
-    return b;
-}
+// function addButton(parent) {
+//     let b = document.createElement('button');
+//     b.setAttribute("id", "Buynow");
+//     b.setAttribute("class", "btn btn-primary");
+//     b.textContent = "Make Payment";
+//     parent.appendChild(b);
+//     return b;
+// }
 
-function addPaymentStatus(parent) {
-    let p = document.createElement('p');
-    p.textContent = "Your Transaction is being Proccessed.....";
-    p.setAttribute("id", "tStatus");
-    parent.appendChild(p);
-    p.addEventListener('purchaseinitiated', purchasedone);
-    p.dispatchEvent(purchaseinitiated);
-    setTimeout(printreciept, 2000);
-
-
-}
-function purchasedone(ev) {
-
-    console.log(ev.type, ev.detail);
-    // Write your PX code here to track the custom events 
-    aptrinsic('track', 'purchaseinitiated', {
-        itemName: ev.detail.itemName,
-        itemBrand: ev.detail.itemBrand
-    });
-}
-
-function printreciept() {
-    var p1 = document.getElementById("tStatus");
-    document.addEventListener('transactionsuccess', transactiondone);
-    document.dispatchEvent(transactionsuccess);
-    p1.textContent = "Transaction Success";
-
-}
+// function addPaymentStatus(parent) {
+//     let p = document.createElement('p');
+//     p.textContent = "Your Transaction is being Proccessed.....";
+//     p.setAttribute("id", "tStatus");
+//     parent.appendChild(p);
+//     p.addEventListener('purchaseinitiated', purchasedone);
+//     p.dispatchEvent(purchaseinitiated);
+//     setTimeout(printreciept, 2000);
 
 
-function transactiondone(ev1) {
+// }
+// function purchasedone(ev) {
 
-    console.log(ev1.type, ev1.detail);
-    // Write your PX code here to track the custom events
-    aptrinsic('track', 'transactionsuccess', {
-        Amountdeducted: ev1.detail.Amountdeducted,
-        Paymentstatus: ev1.detail.Paymentstatus
-    });
-}
+//     console.log(ev.type, ev.detail);
+//     // Write your PX code here to track the custom events 
+//     aptrinsic('track', 'purchaseinitiated', {
+//         itemName: ev.detail.itemName,
+//         itemBrand: ev.detail.itemBrand
+//     });
+// }
+
+// function printreciept() {
+//     var p1 = document.getElementById("tStatus");
+//     document.addEventListener('transactionsuccess', transactiondone);
+//     document.dispatchEvent(transactionsuccess);
+//     p1.textContent = "Transaction Success";
+
+// }
+
+
+// function transactiondone(ev1) {
+
+//     console.log(ev1.type, ev1.detail);
+//     // Write your PX code here to track the custom events
+//     aptrinsic('track', 'transactionsuccess', {
+//         Amountdeducted: ev1.detail.Amountdeducted,
+//         Paymentstatus: ev1.detail.Paymentstatus
+//     });
+// }
 
